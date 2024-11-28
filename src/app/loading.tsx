ff53aa3935e86a1ce5adeedc176b7ff25f759d66
LeQuangHai204 <lequanghai.private@gmail.com>
2024-11-29 01:35:03 +0700

    Add Google OAuth

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,11 +1,11 @@
-export default () => {
+export default function Loading() {
     return (
-        <div className="flex h-screen w-screen items-center justify-center text-2xl">
+        <div className="bg-black flex h-screen w-screen items-center justify-center text-2xl">
             <span className="h-8 w-8 animate-spin rounded-[50%] border-4 border-b-purple-600 border-t-purple-600"></span>
-            <span className="ml-4 text-gray-600">Loading ...</span>
+            <span className="ml-4 text-secondary">Loading ...</span>
         </div>
     );
-};
+}
 
 // loading.tsx only replace page.tsx while loading
 // the layout.tsx and other wrapper components remains interative !
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,10 +3,10 @@ import { Button } from '~/components/ui/button';
 
 export default function NotFound() {
     return (
-        <div className="flex min-h-screen flex-col items-center justify-center p-4 space-y-4">
+        <div className="bg-black flex min-h-screen flex-col items-center justify-center p-4 space-y-4">
             <div className="flex items-center gap-2 text-xl">
-                <span className="text-red-700">404</span>
-                <span>Page not found</span>
+                <span className="text-red-500">404</span>
+                <span className="text-secondary">Page not found</span>
             </div>
             <Button asChild>
                 <Link href="/">Back to Home</Link>
